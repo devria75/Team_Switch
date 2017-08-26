c47bf9683bede78557d5f0f9afac8f1e486ff4ff
azohar99 <aaz36@drexel.edu>
2017-08-25 21:13:28 -0400

    Arden
    Trying to get Team based Raycasting to work

---
+++ Assets/Absorb_Suck.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Absorb_Suck : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        TeamMember tm = GetComponent<TeamMember>();
+        TeamMember myTm = this.GetComponent<TeamMember>();
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+            if (tm.teamID == 0 || myTm.teamID == 0 || tm.teamID != myTm.teamID)
+            {
+                Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+                if (Physics.Raycast(transform.position, fwd, 10))
+                    print("You're hitting stuff!");
+            }
+            }
+
+        }
+    }                                                                                   
---
+++ Assets/TeamMember.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamMember : MonoBehaviour {
+
+    public int teamID = 0;
+
+}
+
